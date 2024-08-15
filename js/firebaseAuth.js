f1a41e8d296398ed42fda59791ee477b55758167
BuildTools <unconfigured@null.spigotmc.org>
2024-08-15 18:09:55 +0200

    add necessary imports to firebaseauth

---
+++ js/firebaseAuth.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlr1B-qkg66Zqkr423UyFrNSLPmScZGIU",
+  authDomain: "transport-clicker-f0d1c.firebaseapp.com",
+  projectId: "transport-clicker-f0d1c",
+  storageBucket: "transport-clicker-f0d1c.appspot.com",
+  messagingSenderId: "177489808647",
+  appId: "1:177489808647:web:b54aeae2843f31ba02c9a2",
+  measurementId: "G-CP6HMGD0N1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
