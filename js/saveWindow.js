340bef3cef2047358790323e7958a6be1d59f660
BuildTools <unconfigured@null.spigotmc.org>
2024-08-15 17:53:06 +0200

    start development of auth system and new saving page

---
+++ js/saveWindow.js
@@ -0,0 +1,6 @@
+const saveWindow = document.getElementById("game-save-window");
+const saveBtn = document.getElementById("nav-item-save-game");
+
+function openSaveWindow() {
+  saveWindow.style.display = "block";
+}
