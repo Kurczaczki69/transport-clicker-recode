475e19782f8d350a5a0ef1c3e44bfcdf18364b04
BuildTools <unconfigured@null.spigotmc.org>
2024-12-09 18:22:55 +0100

    finish credits menu, visual changes, cleanup of codes.js

--- js/creditsMenu.js
+++ js/creditsMenu.js
@@ -9,3 +9,18 @@ creditsBtn.addEventListener("click", () => {
 closeCreditsBtn.addEventListener("click", () => {
   creditsWindow.style.display = "none";
 });
+
+// switching categories in credits menu
+
+const contributorsBtn = document.getElementById("credits-menu-authors-btn");
+const translatorsBtn = document.getElementById("credits-menu-translators-btn");
+
+contributorsBtn.addEventListener("click", () => {
+  document.getElementById("credits-menu-authors-section").style.display = "block";
+  document.getElementById("credits-menu-translators-section").style.display = "none";
+});
+
+translatorsBtn.addEventListener("click", () => {
+  document.getElementById("credits-menu-authors-section").style.display = "none";
+  document.getElementById("credits-menu-translators-section").style.display = "block";
+});
