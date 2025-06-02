20db08bea4ac58c798889ae99c3cf00a873e8d1d
Kurczaczki69 <lukubuku1@onet.pl>
2025-06-02 21:58:31 +0200

    fix

--- src/js/langs.js
+++ src/js/langs.js
@@ -8,6 +8,7 @@ import { populateThemeOptions } from "./settings.js";
 import { initializeCities } from "./data/cityData.js";
 import { playRandomMouseClick } from "./sounds.js";
 import { getI18n } from "./utilities.js";
+import { populateAchievementGrid } from "./achievements/achievementUI.js";
 
 const banana = new Banana();
 
@@ -33,6 +34,7 @@ function updateLang(lang) {
       initializeVehicles();
       initializeCities();
       populateThemeOptions();
+      populateAchievementGrid();
       syncVehiclePrices();
       const currentPage = document.body.getAttribute("data-page");
       document.documentElement.setAttribute("lang", lang);
